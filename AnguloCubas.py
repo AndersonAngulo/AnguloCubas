971f30817295ce1873a94fc55c426efceff381bc
AndersonAngulo <56948485+AndersonAngulo@users.noreply.github.com>
2019-11-03 22:31:51 -0500

    Create AnguloCubas.py

---
+++ AnguloCubas.py
@@ -0,0 +1,318 @@
+#calcular el area de un triangulo
+area,base,altura=0.0,0.0,0.0
+
+#asignacion de valores
+base=20
+altura=10
+
+#calculo
+area=(base*altura)/2
+
+#mostrar valores
+print("base= ",base)
+print("altura= ",altura)
+print("area= ",area)
+
+
+#calcular el area de un rectangulo
+area1,base1,altura1=0.0,0.0,0.0
+
+#asignacion de valores
+base1=30
+altura1=10
+
+#calculo
+area1=(base1*altura1)
+
+#mostrar valores
+print("base= ",base1)
+print("altura= ",altura1)
+print("area= ",area1)
+
+
+#calcular el area de un cuadrado
+lado,area2=0.0,0.0
+
+#asignacion de valores
+lado=5
+
+
+#calculo
+area2=lado*lado
+
+#mostrar valores
+print("lado= ",base)
+print("area= ",area2)
+
+
+#calcular el area de un trapecio
+base_menor,base_mayor,altura2,area3=0.0,0.0,0.0,0.0
+
+#asignacion de valores
+base_menor=15
+base_mayor=35.6
+altura2=6
+
+#calculo
+area=((base_mayor+base_menor)*altura)/2
+
+#mostrar valores
+print("base_menor= ",base_menor)
+print("base_mayor= ",base_mayor)
+print("altura= ",altura2)
+print("area= ",area3)
+
+
+#calcular el area de un rombo
+area4,diagonal1,diagonal2=0.0,0.0,0.0
+
+#asignacion de valores
+diagonal1=6
+diagonal2=12
+
+#calculo
+area=(diagonal1*diagonal2)/2
+
+#mostrar valores
+print("diagonal1= ",diagonal1)
+print("diagonal2= ",diagonal2)
+print("area4= ",area4)
+
+
+#calcular el area de un circulo
+area5,radio,pi=0.0,0.0,3.14159
+
+#asignacion de valores
+radio=42
+
+#calculo
+area=(pi*radio*radio)
+
+#mostrar valores
+print("radio= ",radio)
+print("pi= ",pi)
+print("area= ",area5)
+
+
+#calcular el area de un pararelogramo
+base2,altura3,area6=0.0,0.0,0.0
+
+#asignacion de valores
+base2=16
+altura3=15
+
+#calculo
+area=(base2*altura3)
+
+#mostrar valores
+print("base= ",base2)
+print("altura= ",altura3)
+print("area= ",area6)
+
+
+
+#calcular el area de un poligono regular
+perimetro,apotema,area7=0.0,0.0,0.0
+
+#asignacion de valores
+perimetro=20
+apotema=10
+
+#calculo
+area=(perimetro*apotema)/2
+
+#mostrar valores
+print("perimetro= ",perimetro)
+print("apotema= ",apotema)
+print("area= ",area7)
+
+
+#calcular la distancia con una velocidad constante
+distancia,velocidad,tiempo=0.0,0.0,0.0
+
+#asignacion de valores
+velocidad=20
+tiempo=5.4
+
+#calculo
+distancia=velocidad*tiempo
+
+#mostrar valores
+print("velocidad= ",velocidad)
+print("tiempo= ",tiempo)
+print("distancia= ",distancia)
+
+
+
+#calcular la edad de una persona
+año_actual,año_nacimiento,edad=0.0,0.0,0.0
+
+#asignacion de valores
+año_actual=2002
+año_nacimiento=2019
+
+#calculo
+edad=año_actual-año_nacimiento
+
+#mostrar valores
+print("año_actual= ",año_actual)
+print("año_nacimiento= ",año_nacimiento)
+print("edad= ",edad)
+
+
+
+#calcular el precio total a pagar por cierta cantiddad de productos
+cant_produc,precio_unidad,precio_total=0,0.0,0.0
+
+#asignacion de valores
+cant_produc=20
+precio_unidad=10
+
+#calculo
+precio_total=cant_produc*precio_unidad
+
+#mostrar valores
+print("precio por unidad= ",precio_unidad)
+print("cantidad de productos= ",cant_produc)
+print("precio total= ",precio_total)
+
+
+
+#calcular la cantidad de elementos de una matriz
+filas,columnas,elementos=0,0,0
+
+#asignacion de valores
+filas=4
+columnas=15
+
+#calculo
+elementos=filas*columnas
+
+#mostrar valores
+print("filas= ",filas)
+print("columnas= ",columnas)
+print("elementos= ",elementos)
+
+
+
+#calcular la longitud de una circunferencia
+diamentro,pi1,longi=0.0,3.14159,0.0
+
+#asignacion de valores
+diametro=12.6
+
+#calculo
+longi=diametro*pi1
+#mostrar valores
+print("diametro= ",diametro)
+print("pi= ",pi)
+print("longitud= ",longi)
+
+
+#calcular prcio con igv
+precio_inicial,precio_final,igv=0.0,0.0,0.17
+
+#asignacion de valores
+precio_inicial=1500
+
+#calculo
+precio_final=precio_inicial+(precio_inicial*igv)
+
+#mostrar valores
+print("precio inicial= ",precio_inicial)
+print("igv= ",igv)
+print("precio final= ",precio_final)
+
+
+#calcular z
+x,y,z=0.0,0.0,0.0
+
+#asignacion de valores
+x=3
+y=4
+
+#calculo
+z=(x*x)+(2*x*y)+(y*y)
+
+#mostrar valores
+print("x= ",x)
+print("y= ",y)
+print("z= ",z)
+
+
+#calcular el area de un triangulo
+area,base,altura=0.0,0.0,0.0
+
+#asignacion de valores
+base=20
+altura=10
+
+#calculo
+area=(base*altura)/2
+
+#mostrar valores
+print("base= ",base)
+print("altura= ",altura)
+print("base= ",area)
+
+
+#calcular el numero de monedad de 5 soles para recaudar cierta cantidad
+cant_monedas,cant_dinero,moneda=0,0,5
+
+#asignacion de valores
+cant_dinero=420
+
+#calculo
+cant_monedas=cant_dinero/moneda
+
+#mostrar valores
+print("cantidad de dinero= ",cant_dinero)
+print("moneda= ",moneda)
+print("cant_monedas= ",cant_monedas)
+
+
+#calculo de la superficie total de  un prisma
+area_lateral1,area_base,area_total=0.0,0.0,0.0
+
+#asignacion de valores
+area_lateral=20
+area_base=10
+
+#calculo
+area_total=area_lateral1+(2*area_base)
+
+#mostrar valores
+print("area de la base= ",area_base)
+print("area lateral= ",area_lateral1)
+print("area total= ",area_total)
+
+
+#calcular el area total de un cilindro
+area_circulo,area_lateral,area_total1=0.0,0.0,0.0
+
+#asignacion de valores
+area_circulo=100
+area_lateral=50
+
+#calculo
+area_total1=area_lateral+(2*area_circulo)
+
+#mostrar valores
+print("area_lateral= ",area_lateral)
+print("area_circulo= ",area_circulo)
+print("area_total= ",area_total1)
+
+
+#calcular el area de una piramide
+area_total2,area_base1,area_lateral2=0.0,0.0,0.0
+
+#asignacion de valores
+area_base1=20
+area_lateral2=10
+
+#calculo
+area_total2=area_base1 + area_lateral2
+#mostrar valores
+print("area lateral= ",area_lateral2)
+print("area de la base= ",area_base1)
+print("area total= ",area_total2)
